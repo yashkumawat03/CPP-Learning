965c9b23ba77366d0b71476b231e23c76934f113
yashkumawat03 <yashkumawat886@gmail.com>
2025-09-03 19:44:13 +0530

    Patterns

---
+++ patterns/pattern4.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void pattern3(int n){
+    for(int i = 1; i <= n; i++ ){
+        for(int j=1; j<=i; j++) {
+            cout << i << " ";
+        }
+        cout<<endl;
+    }
+}
+    int main() {
+        int n;
+        cin>>n;
+        pattern3(n);
+        return 0;
+}
