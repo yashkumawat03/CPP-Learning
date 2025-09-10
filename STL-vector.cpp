0fa214ce0717d2aa0bdffc0707bc3109f0251937
yashkumawat03 <yashkumawat886@gmail.com>
2025-09-10 19:30:07 +0530

    C++ STL

--- STL-vector.cpp
+++ STL-vector.cpp
@@ -133,5 +133,230 @@ void explainVector(){
 
     v.clear();  // erase the entire vector
     cout<<v.empty();
+}
+
+
+void explainlist() {
+    list<int> ls;
+    ls.push_back(2);// {2}
+    ls.emplace_back(4); //{2,4}
+
+    ls.push_front(5); //{5,2,4};
+    ls.emplace_front(); {2,4};
+}
+
+
+
+
+
+void explainDeque() {
+     deque<int>dq;
+     dq.push_back(1); // {1}
+     dq.emplace_back(2); // {1,2}
+     dq.push_front(4);//{4,1,2}
+     dq.emplace_front(3);//{3,4,1,2}
+
+     dq.pop_back(); // {3,4,1}
+     dq.pop_front();//{4,1}
+
+     dq.back();
+     dq.front();
+
+     //rest functions same as vector
+     //begin , end , rbegin , rend , clear , insert, size ,swap
+
+}
+
+
+
+void explainPQ(){
+    priority_queue<int>pq;
+
+    pq.push(5);//{5}
+    pq.push(2);//{5,2}
+    pq.push(8);//{8,5,2}
+    pq.emplace(10);//{10,8,5,2}
+
+    cout<< pq.top(); //prints 10
+    pq.pop();//{8,5,2}
+    cout<<pq.top(); // prints 8
+    // size swap empty function same as others
+    // minimum heap
+    priority_queue<int, vector<int>,greater<int>> pq;
+    pq.push(5);//{5}
+    pq.push(2);//{2,5}
+    pq.push(8);//{2,5,8}
+    pq.emplace(10);//{2,5,8,10}
+
+    cout<< pq.top(); //prints 2
+
+    
+}
+
+
+void explainSet(){
+    set<int>st;
+    st.insert(1);//{1}
+    st.emplace(2);//{1,2}
+    st.insert(2);//{1,2}
+    st.insert(4);//{1,2,4}
+    st.insert(3);//{1,2,3,4}
+
+    //Functionality of insert in vector
+    //can be used also, that only increase efficiency
+
+    //begin(), end() , rbegin(), rend(), size(),
+    //empty() and swap() are same as those of above 
+    //{1,2,3,4,5}
+    auto it = st.find(3);
+    //{1,2,3,4,5}
+    auto it = st.find(6);
+    //{1,4,5}
+    st.erase(5); //erases 5 // takes logarithmic time
+    
+    int cnt = st.count(1);
+
+    auto it=st.find(3);
+    st.erase(it);// it takes constant time
+    // {1,2,3,4,5}
+    auto it1 = st.find(2);
+    auto it2 = st.find(4);
+    st.erase(it1,it2); //after erase {1,4,5} {first,last}
+    //lower_bound() and upper_bound() function works in same way as in vector it does.
+    //This is syntax 
+    auto it = st.lower_bound(2);
+    auto it = st.upper_bound(3);
+
+
+}
+
+
+
+
+void explainMultiSet() {
+    //everything is same as set
+    // only stores duplicate elements also 
+
+    multiset<int>ms;
+    ms.insert(1); //{1}
+    ms.insert(1);//{1,1}
+    ms.insert(1); //{1,1,1}
+
+    ms.erased(1); //all 1's erased
+    int cnt = ms.count(1);
+    //only a single one erase
+
+    ms.erase(ms.find(1),ms.find(1)+2);
+    // rest all function same as set
+}
+
+
+
+void explainUSet(){
+    unordered_set<int> st;
+    /*
+    lower_bound() and upper_bound() function doesnot works .
+    rest all functions are same as above
+    it doesnot stores in any partuicular order it has a better complexity
+    than set  in most cases , except some when collision happens 
+    */
+}
+
+
+void explainMap(){
+    map<int , int> mpp;
+    map<int , pair<int,int>> mpp;
+    map<pair<int,int>,int> mpp;
+
+    mpp[1]=2;
+    mpp.emplace({3,1});
+    mpp.insert({2,4});
+    mpp[{2,3}] = 10;
+
+    for(auto it : mpp){
+        cout << it.first <<" "<< it.second << endl;
+
+    }
+    cout<< mpp[1];
+    cout<<mpp[5];
+     auto it = mpp.find(3);
+     cout<< *(it).second;
+
+     auto it = mpp.find(5);
+     // this is the syntax
+    auto it = mpp.lower_bound(2);
+    auto it = mpp.upeer_bound(3);
+    // erase , swap , size , empty are same as above
+
+
+
+
+}
+
+
+
+
+
+void explainMultimap() {
+    // everything smae as map , only it can store multiple keys 
+    // only mpp[key] cannot be used here
+
+}
+
+void explainUnorderedMap() {
+    //same as set and unordered_Set differences
+     
+}
+
+
+bool comp(pair<int,int>p1, pair<int,int>p2) {
+    if(p1.second < p2.second){
+        return true;   
+    } else if(p1.second == p2.second){
+        if(p1.first>p2.second) return true;
+
+    }
+    return false;
+}
+
+
+
+
+void explainExtra(){
+
+    sort(a,a+n);
+    sort(v.begin() ,v.end());
+
+    sort(a+2 , a+4);
+    sort(a,a+n, greater<int>);
+    pair<int,int>a[] = {{1,2},{2,1},{4,1}};
+
+    //sort it acc to second element
+    //if second element is same , then sort 
+    //it acc to first element but in descending
+
+    sort(a,a+n,comp)
+    // {4,1},{2,1},{1,2}};
+
+    int num = 7;
+    int cnt = __builtin_popcount();
+
+    long long num = 165786578687;
+    int cnt = __builtin_popcountll();
+
+    string s = "123";
+    sort(s.begin(),s.end());
+
+    do{
+        cout<< s << endl;
+    } while(next_permutation(s.begin(),s.end()));
+ 
+
+    int maxi = *max_element(a,a+n);
+
+}
+int main(){
+    explainExtra();
+    return 0;
 
 }
