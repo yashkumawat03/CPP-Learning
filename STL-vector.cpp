a9a3272f626f62099f97331794a11b84d94cfac4
yashkumawat03 <yashkumawat886@gmail.com>
2025-09-10 19:27:56 +0530

    C++ STL

--- STL-vector.cpp
+++ STL-vector.cpp
@@ -16,14 +16,16 @@ Iterators
 
 // Pairs
 
+
+
 void explainPair() {
-    pair<int<int>p = {1,3};
+    pair<int,int>p = {1,3};
     cout << p.first << " " << p.second;
     
-    pair<int,pair<int,int>> p = {1,{3,4}}
+    pair<int,pair<int,int>> p = {1,{3,4}};
     cout << p.first << " " << p.second.first << " " << p.second.second;
 
-    pair<int,int>arr[]= {{1,2},{2,5},{5,1}}
+    pair<int,int>arr[]= {{1,2},{2,5},{5,1}};
     cout<<arr[1].second;  // 5
 
 }
