82c4898f9f20ca025c60e457fa06ce8563ac7cec
yashkumawat03 <yashkumawat886@gmail.com>
2025-09-13 00:00:10 +0530

    C++

--- patterns/pattern.cpp
+++ patterns/pattern.cpp
@@ -5,7 +5,7 @@ int main() {
     int n;
     cin >> n;
     for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n; j++){
+        for(int j = 0; j < n; j++){yty
             cout << "*";
         }
     cout << endl;
--- vector.cpp
+++
@@ -1,8 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-int main(){
-    vector<int>v={1,2,3,4};
-    cout<<v[0];
-    return 0;
-}
