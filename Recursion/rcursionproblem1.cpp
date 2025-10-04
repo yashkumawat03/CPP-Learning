c59d99f2cee0b0cfd9c4a3cac1ac537dd791d771
yashkumawat03 <yashkumawat886@gmail.com>
2025-10-04 19:10:21 +0530

    C++ Basics

---
+++ Recursion/rcursionproblem1.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void print(int i,int n){
+    if(i > 3) return;
+    cout << "Yash" << endl;
+    print(i+1,n);
+}
+
+int main(){
+    int n;
+    cin >> n;
+    print(1,n);
+    return 0;
+}
+
