b68022725ad56faa9b8073965afcdd20b6ba4d44
yashkumawat03 <yashkumawat886@gmail.com>
2026-02-06 18:13:34 +0530

    array

--- Array/sorted.cpp
+++ Array/sorted.cpp
@@ -1,17 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-// Function to check if the array is sorted
-bool isSorted(int arr[], int n) {
-    for (int i = 1; i < n; i++) {
-        if (arr[i] < arr[i - 1])  // If any element is smaller than the previous one, return false
+bool issorted(int arr[],int n){
+    for(int i = 0; i<n-1; i++){
+        if(arr[i]>arr[i+1]){
             return false;
+        }
     }
-
-    return true;  // Return true if the array is sorted
+    return true;
 }
 
-int main() {
-    int arr[] = {1, 2, 3, 4, 5}, n = 5;
-    printf("%s", isSorted(arr, n) ? "True" : "False");  // Output result
+int main(){
+    int n;
+    cin >>n;
+    int arr[n];
+    for(int i =0; i<n;i++){
+        cin >> arr[i];
+    }
+
+    bool result = issorted(arr,n);
+    cout << result;
+
+    return 0;
 }
